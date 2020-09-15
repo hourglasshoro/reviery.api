4c19dd62e6b9df607c0460fe35c1f35237b79231
nakajimaibuki <nakajimai@crunchtimer.jp>
2020-09-16 00:13:35 +0900

    add: redis.go

---
+++ src/app/infrastructure/redis/redis.go
@@ -0,0 +1,23 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"os"
+)
+
+var ctx = context.Background()
+
+func NewRedis() (*redis.Client, error) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT")),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       0,
+	})
+	_, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		return nil, err
+	}
+	return rdb, nil
+}
